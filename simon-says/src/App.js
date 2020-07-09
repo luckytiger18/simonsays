96e7856f52c3d988af6ddceebaef98c131565f13
Annie Luc <lannie3186@gmail.com>
2020-07-08 21:51:37 -0700

    initial set up

---
+++ simon-says/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="welcomeTo">Welcome to Simon Says Game
+       <Button>How to Play</Button>
+       <Button>Start Game</Button>
+       </div>
+
+ 
+  );
+}
+
+export default App;
